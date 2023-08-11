a1190a8a0e806c8ba96960fbadf73235a18653da
salehWeb <salehwebdev2004@gmail.com>
2023-08-11 23:38:06 +0200

    fix

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d")
 const cw = canvas.clientWidth;
 const ch = canvas.clientHeight;
-const space = 40;
-const cols = 20
-const rows = 10
+const space = 25; 
 const pises = [
     [
         [true, false],
@@ -38,6 +36,7 @@ const pises = [
     ],
 ];
 
+
 function initMatrix() {
     const continer = document.getElementById("matrix");
     const matrix = [];
@@ -62,55 +61,73 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function drawPice() {
-    const pice = pises[randomInt(0, 4)]
+function drawPice(pw = 0, ph = 0, x = 0, y = 0, pice = pises[randomInt(0, 4)], imgSrc = `./imgs/${randomInt(1, 7)}.png`) {
     const img = new Image();
-    img.src = `./imgs/${randomInt(1, 7)}.png`;
-
-    const offCanvas = document.createElement("canvas");
-    const offCtx = offCanvas.getContext("2d");
-
-    offCanvas.width = 50;
-    offCanvas.height = 100;
-    offCanvas.style.position = "absolute";
-    offCanvas.style.left = "0px";
-    offCanvas.style.top = "0px";
+    img.src = imgSrc;
 
     img.onload = () => {
-        console.log("i work fine");
-        offCtx.drawImage(img, 0, 0, 25, 25);
-        offCtx.drawImage(img, 0, 25, 25, 25);
-        offCtx.drawImage(img, 25, 0, 25, 25);
-        offCtx.drawImage(img, 25, 25, 25, 25);
+        const computeWandH = (pw === 0 || ph === 0);
+        const toDraw = [];
 
-        let x = 25;
-        let y = 50;
-        let w = 50;
-        let h = 100;
-        let a = 0;
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (pice[i][j]) {
+                    if (computeWandH) {
+                        const w = (j + 1) * 25
+                        const h = (i + 1) * 25
+                        if (w > pw) pw = w;
+                        if (h > ph) ph = h;
+                    }
+
+                    toDraw.push({ x: 25 * j, y: 25 * i });
+                }
+            }
+        }
+
+        const offCanvas = document.createElement("canvas");
+        const offCtx = offCanvas.getContext("2d");
+
+        offCanvas.width = pw;
+        offCanvas.height = ph;
+        offCanvas.style.position = "absolute";
+        offCanvas.style.left = "0px";
+        offCanvas.style.top = "0px";
+
+        for (let i = 0; i < toDraw.length; i++) {
+            offCtx.drawImage(img, toDraw[i].x, toDraw[i].y, 25, 25);
+        }
 
         function drawComposite() {
+            console.log("x: " + x + " y: " + y);
+            console.log("pw: " + pw + " ph: " + ph)
+
             ctx.clearRect(0, 0, cw, ch);
             ctx.save();
             ctx.translate(x, y);
-            ctx.rotate(a);
-            ctx.drawImage(offCanvas, -w / 2, -h / 2, w, h);
+            ctx.drawImage(offCanvas, 0, 0, pw, ph);
             ctx.restore();
         }
 
+        function canRotate() {
+            return (x < (250 - ph) && y < (500 - pw))
+        }
 
         drawComposite();
 
-        window.addEventListener("keydown", e => {
-            if (e.key === "ArrowLeft") x -= 25;
-            if (e.key === "ArrowRight") x += 25;
-            if (e.key === "ArrowUp") y -= 25;
-            if (e.key === "ArrowDown") y += 25;
-            if (e.key === "A") a -= 0.1;
-            if (e.key === "D") a += 0.1;
+        const callback = (e) => {
+            if (e.key === "ArrowLeft" && x > 0) x -= 25;
+            if (e.key === "ArrowRight" && x < (250 - pw)) x += 25;
+            if (e.key === "ArrowUp" && y > 0) y -= 25;
+            if (e.key === "ArrowDown" && y < (500 - ph)) y += 25;
+            if (e.key === "A" && canRotate()) {
+                window.removeEventListener("keydown", callback)
+                drawPice(ph, pw, x, y, rotateMatrix(pice), imgSrc)
+            }
 
             drawComposite();
-        });
+        }
+
+        window.addEventListener("keydown", callback);
     }
 }
 
