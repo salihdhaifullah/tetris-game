e4776c1a24abeb9ef98f9a65dce594e55bb5f7cc
salehWeb <salehwebdev2004@gmail.com>
2023-08-13 19:14:36 +0200

    fully moveable pices

--- index.js
+++ index.js
@@ -1,59 +1,107 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d")
-const cw = canvas.clientWidth;
-const ch = canvas.clientHeight;
-const space = 25; 
-const pises = [
+const continer = document.getElementById("matrix");
+const mw = continer.clientWidth;
+const mh = continer.clientHeight;
+const space = mw / 10;
+
+const pice1 = [
     [
-        [true, false],
-        [true, false],
-        [true, false],
-        [true, false],
+        [true],
+        [true],
+        [true],
+        [true],
     ],
     [
+        [true, true, true, true]
+    ]
+];
+
+const pice2 = [
+    [
         [true, true],
-        [true, true],
-        [false, false],
-        [false, false],
-    ],
+        [true, true]
+    ]
+];
+
+const pice3 = [
     [
         [true, false],
         [true, false],
-        [true, true],
-        [false, false],
+        [true, true]
     ],
     [
-        [true, false],
-        [true, true],
-        [true, false],
-        [false, false],
+        [true, true, true],
+        [true, false, false]
     ],
     [
-        [true, false],
         [true, true],
         [false, true],
-        [false, false],
+        [false, true]
+    ],
+    [
+        [false, false, true],
+        [true, true, true]
+    ]
+];
+
+const pice4 = [
+    [
+        [true, false],
+        [true, true],
+        [true, false]
+    ],
+    [
+        [true, true, true],
+        [false, true, false]
+    ],
+    [
+        [false, true],
+        [true, true],
+        [false, true]
+    ],
+    [
+        [false, true, false],
+        [true, true, true]
+    ]
+];
+
+const pice5 = [
+    [
+        [true, false],
+        [true, true],
+        [false, true]
+    ],
+    [
+        [false, true, true],
+        [true, true, false]
     ],
 ];
 
+const pice6 = [
+    [
+        [false, true],
+        [true, true],
+        [true, false]
+    ],
+    [
+        [true, true, false],
+        [false, true, true]
+    ],
+]
+
+const pises = [pice1, pice2, pice3, pice4, pice5, pice6];
 
 function initMatrix() {
-    const continer = document.getElementById("matrix");
-    const matrix = [];
     for (let i = 0; i < 20; i++) {
-        matrix.push([])
         for (let j = 0; j < 10; j++) {
-            matrix[i].push(false)
             const id = `${i}-${j}`;
             const ele = document.createElement("span");
             ele.id = id;
             continer.append(ele);
         }
     }
-    return matrix;
-}
+};
 
-const matrix = initMatrix();
+initMatrix();
 
 function randomInt(min, max) {
     min = Math.ceil(min);
@@ -61,74 +109,88 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function drawPice(pw = 0, ph = 0, x = 0, y = 0, pice = pises[randomInt(0, 4)], imgSrc = `./imgs/${randomInt(1, 7)}.png`) {
-    const img = new Image();
-    img.src = imgSrc;
-
-    img.onload = () => {
-        const computeWandH = (pw === 0 || ph === 0);
-        const toDraw = [];
-
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 4; j++) {
-                if (pice[i][j]) {
-                    if (computeWandH) {
-                        const w = (j + 1) * 25
-                        const h = (i + 1) * 25
-                        if (w > pw) pw = w;
-                        if (h > ph) ph = h;
-                    }
-
-                    toDraw.push({ x: 25 * j, y: 25 * i });
-                }
+function clearPrevPice(prevX, prevY, w, h, pice) {
+    for (let i = 0; i < pice.length; i++) {
+        for (let j = 0; j < pice[i].length; j++) {
+            if (pice[i][j]) {
+                const prevPostion = `${i + prevY - h}-${j + prevX - w}`;
+                document.getElementById(prevPostion).style.backgroundImage = "none"
             }
         }
-
-        const offCanvas = document.createElement("canvas");
-        const offCtx = offCanvas.getContext("2d");
-
-        offCanvas.width = pw;
-        offCanvas.height = ph;
-        offCanvas.style.position = "absolute";
-        offCanvas.style.left = "0px";
-        offCanvas.style.top = "0px";
-
-        for (let i = 0; i < toDraw.length; i++) {
-            offCtx.drawImage(img, toDraw[i].x, toDraw[i].y, 25, 25);
-        }
-
-        function drawComposite() {
-            console.log("x: " + x + " y: " + y);
-            console.log("pw: " + pw + " ph: " + ph)
-
-            ctx.clearRect(0, 0, cw, ch);
-            ctx.save();
-            ctx.translate(x, y);
-            ctx.drawImage(offCanvas, 0, 0, pw, ph);
-            ctx.restore();
-        }
-
-        function canRotate() {
-            return (x < (250 - ph) && y < (500 - pw))
-        }
-
-        drawComposite();
-
-        const callback = (e) => {
-            if (e.key === "ArrowLeft" && x > 0) x -= 25;
-            if (e.key === "ArrowRight" && x < (250 - pw)) x += 25;
-            if (e.key === "ArrowUp" && y > 0) y -= 25;
-            if (e.key === "ArrowDown" && y < (500 - ph)) y += 25;
-            if (e.key === "A" && canRotate()) {
-                window.removeEventListener("keydown", callback)
-                drawPice(ph, pw, x, y, rotateMatrix(pice), imgSrc)
-            }
-
-            drawComposite();
-        }
-
-        window.addEventListener("keydown", callback);
     }
 }
 
-drawPice()
+function redrawPice(x, y, w, h, pice, img) {
+    for (let i = 0; i < pice.length; i++) {
+        for (let j = 0; j < pice[i].length; j++) {
+            if (pice[i][j]) {
+                const position = `${i + y - h}-${j + x - w}`;
+                document.getElementById(position).style.backgroundImage = `url("${img}")`
+            }
+        }
+    }
+}
+
+function canRotate(x, y, w, h) {
+    const newX = x + (w - h);
+    const newY = y + (h - w);
+    if (newX < w || newX > 10) return false;
+    if (newY < h || newY > 20) return false;
+    return true;
+}
+
+function drawPiece() {
+    const img = `./imgs/${randomInt(1, 7)}.png`;
+    const item = randomInt(0, pises.length - 1)
+    let currentRotateing = randomInt(0, pises[item].length - 1)
+    let piece = pises[item][currentRotateing]
+    let h = piece.length;
+    let w = piece[0].length;
+    let y = h;
+    let x = w;
+
+    redrawPice(x, y, w, h, piece, img);
+
+    const callback = (e) => {
+        if (e.key === "ArrowLeft" && x > w) {
+            clearPrevPice(x, y, w, h, piece)
+            x--;
+            redrawPice(x, y, w, h, piece, img);
+        };
+        if (e.key === "ArrowRight" && x < 10) {
+            clearPrevPice(x, y, w, h, piece);
+            x++;
+            redrawPice(x, y, w, h, piece, img);
+        };
+        if (e.key === "ArrowUp" && y > h) {
+            clearPrevPice(x, y, w, h, piece)
+            y--;
+            redrawPice(x, y, w, h, piece, img);
+        };
+        if (e.key === "ArrowDown" && y < 20) {
+            clearPrevPice(x, y, w, h, piece)
+            y++;
+            redrawPice(x, y, w, h, piece, img);
+        };
+        if (e.key === "A") {
+            let newRotateing = currentRotateing;
+            if (pises[item].length - 1 === newRotateing) newRotateing = 0;
+            else newRotateing++;
+            
+            const rotatedPiece = pises[item][newRotateing]
+            if (canRotate(x, y, h, w)) {
+                clearPrevPice(x, y, w, h, piece);
+                [w, h] = [h, w];
+                y += h - w
+                x += w - h
+                redrawPice(x, y, w, h, rotatedPiece, img);
+                piece = rotatedPiece;
+                currentRotateing = newRotateing;
+            }
+        };
+    }
+
+    window.addEventListener("keydown", callback);
+}
+
+drawPiece()
