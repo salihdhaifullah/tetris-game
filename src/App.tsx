c29870e3dcd98cf4f6b1f6b9fdc091fb3e79e41f
salehWeb <salehwebdev2004@gmail.com>
2023-08-25 20:33:48 +0200

    use canvse

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { MutableRefObject, useCallback, useEffect, useRef, useState } from "react";
- 
+
+// TODO: add the next four pices in page
 // TODO: hilghting the pices that they will be remove
 // TODO: implement hard drop hold util resh the end
-// TODO: decrees time of going down when level is incrressed
+// TODO: add levels and decrees time of going down when level is incrressed
 // FIX: clean up
-// PERF: optimization 
-
+// PERF: optimization
+ 
 const pice1 = [
     [
         [true],
@@ -158,8 +159,8 @@ interface Ihilght {
     pice: boolean[][]
 }
 
-function loadImage(src: string, space: number): Promise<HTMLImageElement> {
-    return new Promise(resolve => {
+async function loadImage(src: string, space: number): Promise<HTMLImageElement> {
+    return await new Promise(resolve => {
         const img = new Image(space, space);
         img.src = src;
         img.onload = () => resolve(img);
@@ -169,35 +170,34 @@ function loadImage(src: string, space: number): Promise<HTMLImageElement> {
 function App() {
     const [isGame, setIsGame] = useState(false);
     const [score, setScore] = useState(0);
-    const [space, setSpace] = useState(0)
     const [level, setLevel] = useState(1);
     const [stack, setStack] = useState<Iitem[]>([]);
     const [stopedPices, setStopedPices] = useState<boolean[][]>([]);
-    const [hilghtPosition, setHilghtPosition] = useState<Ihilght | null>(null);
+    const hilghtPosition = useRef<Ihilght | null>(null);
 
     const ctx: MutableRefObject<CanvasRenderingContext2D | null> = useRef(null)
-
+    const space = useRef(0)
+    
     const canvesCallback = useCallback((ele: HTMLCanvasElement) => {
-        setSpace(ele.clientWidth / 10);
+        space.current = ele.clientWidth / 10;
         ctx.current = ele.getContext("2d")
     }, [])
 
     async function startGame() {
-        initMatrix();
-        drawPiece();
-        const temp: Iitem[] = [];
+        setStopedPices(initMatrix());
         for (let i = 0; i < 4; i++) {
-            temp.push(await getItem())
+            stack.push(await getItem())
         }
-        setStack(temp)
+        setIsGame(true)
     }
 
     useEffect(() => {
-        if (isGame) startGame()
+        if (isGame) drawPiece();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isGame])
 
     async function getItem(): Promise<Iitem> {
-        const img = await loadImage(`./assets/${randomInt(1, 7)}.png`, space);
+        const img = await loadImage(`/assets/${randomInt(1, 7)}.png`, space.current);
         const item = randomInt(0, pises.length - 1)
         const currentRotating = randomInt(0, pises[item].length - 1)
         const piece = pises[item][currentRotating]
@@ -234,18 +234,18 @@ function App() {
         for (let i = 0; i < pice.length; i++) {
             for (let j = 0; j < pice[i].length; j++) {
                 if (pice[i][j]) {
-                    ctx.current?.clearRect((j + prevX - w) * space, (i + prevY - h) * space, w * space, h * space)
+                    ctx.current?.clearRect((j + prevX - w) * space.current, (i + prevY - h) * space.current, space.current, space.current)
                 }
             }
         }
     }
 
     function clearPrevHilght() {
-        if (hilghtPosition !== null) {
-            for (let i = 0; i < hilghtPosition.pice.length; i++) {
-                for (let j = 0; j < hilghtPosition.pice[i].length; j++) {
-                    if (hilghtPosition.pice[i][j]) {
-                        ctx.current?.clearRect((j + hilghtPosition.x - hilghtPosition.w) * space, (i + hilghtPosition.y - hilghtPosition.h) * space, hilghtPosition.w * space, hilghtPosition.h * space)
+        if (hilghtPosition.current !== null) {
+            for (let i = 0; i < hilghtPosition.current.pice.length; i++) {
+                for (let j = 0; j < hilghtPosition.current.pice[i].length; j++) {
+                    if (hilghtPosition.current.pice[i][j]) {
+                        ctx.current?.clearRect((j + hilghtPosition.current.x - hilghtPosition.current.w) * space.current, (i + hilghtPosition.current.y - hilghtPosition.current.h) * space.current, space.current, space.current)
                     }
                 }
             }
@@ -266,29 +266,30 @@ function App() {
                 if (pice[i][j]) {
                     ctx.current?.save();
                     ctx.current!.globalAlpha = 0.5;
-                    ctx.current?.drawImage(img, (j + x - w) * space, (i + newY - h) * space);
+                    ctx.current?.drawImage(img, (j + x - w) * space.current, (i + newY - h) * space.current);
                     ctx.current?.restore();
                 }
             }
         }
 
-        setHilghtPosition({ y: newY, x, w, h, pice });
+        hilghtPosition.current = { y: newY, x, w, h, pice };
     }
 
     function removeLine(line: number) {
         for (let i = 0; i < 10; i++) {
             stopedPices[line][i] = false;
-            ctx.current?.clearRect((i) * space, (line) * space, space, space)
+            ctx.current?.clearRect(i * space.current, line * space.current, space.current, space.current)
         }
     }
 
-    function reArrangeLines(line: number, linesCount: number, img: HTMLImageElement) {
+    function reArrangeLines(line: number, linesCount: number) {
         let i = line;
         while (i > (linesCount - 1)) {
             for (let j = 0; j < 10; j++) {
                 if (!stopedPices[i - linesCount][j]) continue;
-                ctx.current?.clearRect((j) * space, (i-linesCount) * space, space, space)
-                ctx.current?.drawImage(img, j, i)
+                const imageData = ctx.current?.getImageData(j * space.current, (i - linesCount) * space.current, space.current, space.current); 
+                ctx.current?.clearRect(j * space.current, (i - linesCount) * space.current, space.current, space.current)
+                ctx.current?.putImageData(imageData!, j*space.current, i*space.current);           
                 stopedPices[i - linesCount][j] = false;
                 stopedPices[i][j] = true;
             }
@@ -296,7 +297,7 @@ function App() {
         }
     }
 
-    function checkLine(img: HTMLImageElement) {
+    function checkLine() {
         let lastLine = -1;
         let linesCount = 0;
 
@@ -311,14 +312,14 @@ function App() {
         }
 
         if (linesCount > 0) calcPoints(linesCount);
-        if (lastLine > -1) reArrangeLines(lastLine, linesCount, img);
+        if (lastLine > -1) reArrangeLines(lastLine, linesCount);
     }
 
     function redrawPice(x: number, y: number, w: number, h: number, pice: boolean[][], img: HTMLImageElement) {
         for (let i = 0; i < pice.length; i++) {
             for (let j = 0; j < pice[i].length; j++) {
                 if (pice[i][j]) {
-                    ctx.current?.drawImage(img, j + x - w, i + y - h);
+                    ctx.current?.drawImage(img, (j + x - w) * space.current, (i + y - h) * space.current);
                 }
             }
         }
@@ -353,14 +354,15 @@ function App() {
             }
         }
 
-        setHilghtPosition(null);
+        hilghtPosition.current = null;
         checkLine();
     }
 
-
     async function drawPiece() {
+        // WARNING: use class to create and store this data
         // eslint-disable-next-line prefer-const
-        let { img, item, currentRotating, piece, h, w, y, x } = stack.pop()!;
+        let { img, item, currentRotating, piece, h, w, y, x } = stack.shift()!;
+        stack.push(await getItem());
 
         if (!canGoTo(x, y, w, h, piece)) {
             stopGame()
@@ -371,6 +373,8 @@ function App() {
         redrawPice(x, y, w, h, piece, img);
 
         const callback = (e: KeyboardEvent) => {
+            e.preventDefault()
+            
             if (e.key === "ArrowLeft" && x > w && canGoTo(x - 1, y, w, h, piece)) {
                 clearPrevPice(x, y, w, h, piece)
                 x--;
@@ -438,33 +442,33 @@ function App() {
     }
 
     return (
-        <div className="continer">
-            <div id="matrix" className="game">
+        <div className="gap-5 flex justify-center items-center w-full flex-row h-auto min-h-full">
+            <div className="bg-black w-[250px] h-[500px]">
                 {isGame ? (
-                    <canvas ref={canvesCallback}> </canvas>
+                    <canvas ref={canvesCallback} width={250} height={500} className="w-full h-full"> </canvas>
                 ) : (
-                    <button className="start_game" onClick={() => setIsGame(true)}>
+                    <button className="bg-green-400 text-center border-gray-800 cursor-pointer p-1 text-zinc-100 rounded-md text-xl" onClick={async () => await startGame()}>
                         start game
                     </button>
                 )}
             </div>
 
-            <div className="side">
+            <div className="gap-5 justify-between items-center flex flex-col h-[500px]">
 
                 {isGame ? (
                     <>
-                        <div className="score">
-                            <p>score</p>
-                            <span id="score">{score}</span>
+                        <div className="bg-zinc-100 p-0.5 rounded-md flex flex-col gap-0.5 border-gray-800">
+                            <p className="p-1 text-xl border-gray-800 text-center rounded-md w-full bg-white">score</p>
+                            <span className="p-1 text-2xl border-gray-800 text-center w-full rounded-md h-[200%] bg-white">{score}</span>
                         </div>
 
-                        <div className="pieces" id="pieces">
+                        <div className="bg-zinc-100 flex gap-4 p-2 rounded-md flex-col border-gray-800">
                             {stack.map((item, i) => (
-                                <div key={i}>
+                                <div key={i} className="bg-zinc-100 border-gray-800 flex flex-col">
                                     {item.piece.map((row, j) => (
-                                        <div key={j}>
+                                        <div key={j} className="bg-zinc-100 flex flex-row border-gray-800">
                                             {row.map((val, idx) => (
-                                                <span key={idx} style={{ width: space + "px", height: space + "px", backgroundImage: val ? `url("${item.img.src}")` : "none" }}></span>
+                                                <span key={idx} className="bg-zinc-100 flex flex-col border-gray-800" style={{ width: space.current + "px", height: space.current + "px", backgroundImage: val ? `url("${item.img.src}")` : "none" }}></span>
                                             ))}
                                         </div>
                                     ))}
@@ -475,7 +479,7 @@ function App() {
                 ) : (
                     <>
                         <label htmlFor="select-level">Select Level</label>
-                        <select id="select-level" onChange={(e) => setLevel(e.target.value)}>
+                        <select id="select-level" onChange={(e) => setLevel(Number(e.target.value))}>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
@@ -486,10 +490,10 @@ function App() {
                 )}
 
 
-                <div className="buttons">
+                <div className="flex items-center justify-center flex-col gap-1">
 
                     <div className="row_1">
-                        <button onClick={() => arrow("ArrowUp")}>
+                        <button onClick={() => arrow("ArrowUp")} className="bg-green-400 text-center border-gray-800 cursor-pointer w-14 h-7 p-1 fill-zinc-100 rounded-md text-xl">
                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                                 <path
                                     d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -497,23 +501,23 @@ function App() {
                         </button>
                     </div>
 
-                    <div className="row_2">
+                    <div className="gap-1 flex flex-col">
 
-                        <button onClick={() => arrow("ArrowLeft")}>
+                        <button onClick={() => arrow("ArrowLeft")} className="bg-green-400 text-center border-gray-800 cursor-pointer w-14 h-7 p-1 fill-zinc-100 rounded-md text-xl">
                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
                                 <path
                                     d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
                             </svg>
                         </button>
 
-                        <button onClick={() => arrow("ArrowDown")}>
+                        <button onClick={() => arrow("ArrowDown")} className="bg-green-400 text-center border-gray-800 cursor-pointer w-14 h-7 p-1 fill-zinc-100 rounded-md text-xl">
                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                                 <path
                                     d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
                             </svg>
                         </button>
 
-                        <button onClick={() => arrow("ArrowRight")}>
+                        <button onClick={() => arrow("ArrowRight")} className="bg-green-400 text-center border-gray-800 cursor-pointer w-14 h-7 p-1 fill-zinc-100 rounded-md text-xl">
                             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
                                 <path
                                     d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
