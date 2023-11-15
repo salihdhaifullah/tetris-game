c8063e779ef3071f10e3a5c4ddc9add439f2db99
salehWeb <salehwebdev2004@gmail.com>
2023-11-15 03:57:44 +0200

    convert to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            },
+            manifest: {
+                name: 'Tetris Game',
+                short_name: 'Tetris',
+                theme_color: '#ffffff',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
 })
